6386067e2969f55ce3d402dab039928c13ccb106
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-24 18:47:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.getElementById("submit").addEventListener("click", function(event){
+  event.preventDefault();
+   let val = document.getElementById('new-task-description').value;
+   let li = document.createElement('li');
+   li.innerHTML = val;
+   let result = document.getElementById('tasks');
+   result.appendChild(li)
+ });
+
 });
+function deleteMe(){
+  let val = document.querySelectorAll ('li');
+  if(val.length >0 ){
+  val[val.length - 1].remove();}
+  
+}
