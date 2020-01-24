6e5ebb2fa5d3310d2e4155579e962f3f26fe1264
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-24 18:44:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.getElementById("submit").addEventListener("click", function(event){
+  event.preventDefault();
+   let val = document.getElementById('new-task-description').value;
+   let li = document.createElement('li');
+   li.innerHTML = val;
+   let result = document.getElementById('tasks');
+   result.appendChild(li)
+ });
+
 });
+function deleteMe(){
+  let val = document.querySelectorAll ('tasks li');
+  val[val.length - 1].remove();
+  
+}
